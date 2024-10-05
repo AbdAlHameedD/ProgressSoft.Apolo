1cafb95855225d35bcb5e52f7f967a8e2fdd5339
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-05 19:44:23 +0300

    Add GenerateResultFromDifferentResultType, and GenerateSuccessResult methods

--- ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Helpers/ResultHelper.cs
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Helpers/ResultHelper.cs
@@ -1,13 +1,53 @@
-﻿namespace ProgressSoft.Apolo.Application;
+﻿using System.Net.NetworkInformation;
+using AutoMapper;
+
+namespace ProgressSoft.Apolo.Application;
 
 public class ResultHelper
 {
-    public static Result<T> GenerateFailedResult<T>(Exception ex)
+    private readonly IMapper _mapper;
+
+    public ResultHelper(IMapper mapper) 
+    {
+        _mapper = mapper;
+    }
+
+    public Result<T> GenerateFailedResult<T>(Exception ex)
     {
         return new Result<T>
         {
             Status = OperationStatus.Failed,
             Message = ex.Message
         };
+    }
+
+    public Result<T> GenerateSuccessResult<T>(T data)
+    {
+        return new Result<T>
+        {
+            Status = OperationStatus.Success,
+            Data = data
+        };
+    }
+
+    public Result<T> GenerateResultFromDifferentResultType<T, U>(Result<U> result)
+    {
+        try
+        {
+            return new Result<T>
+            {
+                Status = result.Status,
+                Data = _mapper.Map<T>(result.Data),
+                Message = result.Message
+            };
+        }
+        catch (Exception ex)
+        {
+            return new Result<T>
+            {
+                Status = OperationStatus.Failed,
+                Message = ex.Message
+            };
+        }
     }
 }
