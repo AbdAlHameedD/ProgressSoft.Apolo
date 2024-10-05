a36351bdc2fba73ccb7a65048f3f24b0df49a96b
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-05 20:28:59 +0300

    Add Business card model

---
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Models/BusinessCardModel.cs
@@ -0,0 +1,15 @@
+﻿using ProgressSoft.Apolo.Domain;
+
+namespace ProgressSoft.Apolo.Application;
+
+public class BusinessCardModel
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required Gender Gender { get; set; }
+    public required DateOnly BirthOfDate { get; set; }
+    public required string Email { get; set; }
+    public required string Phone { get; set; }
+    public string? Photo { get; set; }
+    public required string Address { get; set; }
+}
