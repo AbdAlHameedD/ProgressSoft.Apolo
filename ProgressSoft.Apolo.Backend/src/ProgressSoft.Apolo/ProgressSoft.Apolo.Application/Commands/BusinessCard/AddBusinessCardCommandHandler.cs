a54b6bf6fd18a9548149bf338ca3c2552973c63b
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-05 20:27:34 +0300

    Implement add business card command

---
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Commands/BusinessCard/AddBusinessCardCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace ProgressSoft.Apolo.Application;
+
+public class AddBusinessCardCommandHandler : ICommandHandler<AddBusinessCardCommand, Result<BusinessCardModel>>
+{
+    private readonly IBusinessCardService _businessCardService;
+
+    public AddBusinessCardCommandHandler(IBusinessCardService businessCardService)
+    {
+        _businessCardService = businessCardService;
+    }
+
+    public Result<BusinessCardModel> Handle(AddBusinessCardCommand command)
+    {
+        return _businessCardService.Add(command);
+    }
+}
