f8f2766222e5b2e3c28af289bc3c427edca18bd9
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-05 20:28:32 +0300

    Add business card auto mapper

---
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Interfaces/ICommandHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace ProgressSoft.Apolo.Application;
+
+public interface ICommandHandler<TCommand, TResult>
+{
+    TResult Handle(TCommand command);
+}
---
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Mappings/BusinessCardMappings.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using AutoMapper;
+using ProgressSoft.Apolo.Domain;
+
+namespace ProgressSoft.Apolo.Application;
+
+public class BusinessCardMappingProfile : Profile
+{
+    public BusinessCardMappingProfile()
+    {
+        CreateMap<BusinessCard, BusinessCardModel>()
+            .ForMember(dest => dest.Photo, opt => opt.MapFrom(src => (src.Photo != null) ? src.Photo.ToString() : null))
+            .ReverseMap()
+            .ForMember(dest => dest.Photo, opt => opt.MapFrom(src => (src.Photo != null) ? Encoding.UTF8.GetBytes(src.Photo) : null));
+
+        CreateMap<AddBusinessCardCommand, BusinessCard>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => 0))
+            .ForMember(dest => dest.Photo, opt => opt.MapFrom(src => (src.Photo != null) ? Encoding.UTF8.GetBytes(src.Photo) : null));
+    }
+}
