5a4d0083b21fc0d2c987e0fb1c62964af1cd15ad
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-09 14:20:25 +0300

    Change title and modify unit app component unit test

--- ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/app/app.component.ts
+++ ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { BusinessCardFilter } from '../dtos/business-card-filter.dto';
+import { BusinessCard } from '../models/business-card.model';
+import { Result } from '../models/result';
+import { BusinessCardService } from '../services/business-card.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: false
 })
 export class AppComponent {
-  title = 'progressoft-apolo-frontend';
+  title = 'Apolo';
 }
