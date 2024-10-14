e639854b5550373ba69989a9491318160099bd01
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-14 12:13:13 +0300

    Rename mapper file

---
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Mappings/BusinessCardMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ProgressSoft.Apolo.Domain;
+
+namespace ProgressSoft.Apolo.Application;
+
+public class BusinessCardMappingProfile : Profile
+{
+    public BusinessCardMappingProfile()
+    {
+        AllowNullCollections = true;
+
+        CreateMap<BusinessCard, BusinessCardModel>();
+
+        CreateMap<AddBusinessCardCommand, BusinessCard>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => 0));
+    }
+}
