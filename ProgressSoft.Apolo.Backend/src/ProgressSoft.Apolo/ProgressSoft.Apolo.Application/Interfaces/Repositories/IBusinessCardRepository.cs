321f5b989b66bd2ba43274edfbc7a76b7ceae817
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-14 11:34:48 +0300

    Rename file

---
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Interfaces/Repositories/IBusinessCardRepository.cs
@@ -0,0 +1,9 @@
+﻿using ProgressSoft.Apolo.Domain;
+
+namespace ProgressSoft.Apolo.Application;
+
+public interface IBusinessCardRepository : IBaseRepository<BusinessCard>
+{
+    Result<IQueryable<BusinessCard>> Get(BusinessCardFilter filter);
+    Result<BusinessCard> FindById(int id);
+}
