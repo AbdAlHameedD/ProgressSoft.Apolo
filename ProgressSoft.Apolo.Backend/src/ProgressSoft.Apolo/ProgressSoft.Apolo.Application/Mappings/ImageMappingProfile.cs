d684db21d2ade47666cbf6905dd05d94c8c92ddc
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-14 12:13:27 +0300

    Implement Image auto mapper

---
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolo.Application/Mappings/ImageMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ProgressSoft.Apolo.Application.Models;
+using ProgressSoft.Apolo.Domain;
+
+namespace ProgressSoft.Apolo.Application.Mappings
+{
+    public class ImageMappingProfile : Profile
+    {
+        public ImageMappingProfile()
+        {
+            AllowNullCollections = true;
+
+            CreateMap<Image, ImageModel>()
+                .ForMember(src => src.BusinessCards, opt => opt.MapFrom(dest => dest.BusinessCards))
+                .ReverseMap();
+        }
+    }
+}
