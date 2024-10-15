b889c55d59ff586c42a5526086f0a3ec60fc76c7
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-15 13:26:40 +0300

    Implement Image Controller

---
+++ ProgressSoft.Apolo.Backend/src/ProgressSoft.Apolo/ProgressSoft.Apolor.API/Controllers/ImageController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgressSoft.Apolo.Application.Interfaces.Services;
+using ProgressSoft.Apolo.Application.Models;
+
+namespace ProgressSoft.Apolor.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : Controller
+    {
+        private readonly IImageService _imageService;
+
+        public ImageController(IImageService imageService)
+        {
+            _imageService = imageService;
+
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Add([FromBody] ImageModel image)
+        {
+            try
+            {
+                return Ok(_imageService.Add(image));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            try
+            {
+                return Ok(_imageService.Delete(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(_imageService.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
