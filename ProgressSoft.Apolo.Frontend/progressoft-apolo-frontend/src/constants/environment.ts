6668ef48650995b271cc6024eae0e549c8542422
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-16 10:36:56 +0300

    Declare system date format

--- ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/constants/environment.ts
+++ ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/constants/environment.ts
@@ -1,6 +1,18 @@
 export class Environment {
-    public static API_PROTOCOL: string = 'http';
-    public static API_HOST: string = 'localhost';
-    public static API_PORT: number = 5205;
-    public static WEBSITE_TITLE: string = 'Apolo';
+    public static readonly API_PROTOCOL: string = 'http';
+    public static readonly API_HOST: string = 'localhost';
+    public static readonly API_PORT: number = 5205;
+    public static readonly WEBSITE_TITLE: string = 'Apolo';
+
+    public static readonly DATE_FORMATS = {
+        parse: {
+          dateInput: 'LL',
+        },
+        display: {
+          dateInput: 'YYYY-MM-DD',
+          monthYearLabel: 'YYYY',
+          dateA11yLabel: 'LL',
+          monthYearA11yLabel: 'YYYY',
+        },
+      };
 }
