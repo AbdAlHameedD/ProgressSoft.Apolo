9f014c6185fa61cb3335a15436c6155765cf4d7a
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-16 10:51:10 +0300

    Configure routes

--- ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/app/app-routing.module.ts
+++ ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared-components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: '/home/businesscard',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Apolo - Page Not Found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/app/home/home-routing.module.ts
+++ ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/app/home/home-routing.module.ts
@@ -3,7 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { BusinessCardComponent } from './business-card/business-card.component';
 import { NotFoundComponent } from '../shared-components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'businesscard',
+    component: BusinessCardComponent,
+    title: 'Apolo - Business Card'
+  },
+  {
+    path: '',
+    redirectTo: '/home/businesscard',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Apolo - Page Not Found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
