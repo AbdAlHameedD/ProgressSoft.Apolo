13eb171e579bd90a4a5793913b1878e6feb04ce0
Abd Al Hameed Al-Dalgamouni <dabdalhammed6@gmail.com>
2024-10-16 10:48:16 +0300

    Add providers for date adapter, image service, and import not found component

--- ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/app/app.module.ts
+++ ProgressSoft.Apolo.Frontend/progressoft-apolo-frontend/src/app/app.module.ts
@@ -5,27 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { NavbarComponent } from './shared-components/navbar/navbar.component';
-import { BusinessCardComponent } from './business-card/business-card.component';
 import { BusinessCardService } from '../services/business-card.service';
 import { FooterComponent } from './shared-components/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, provideNativeDateAdapter} from '@angular/material/core';
+import { Environment } from '../constants/environment';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+import { ImageService } from '../services/image.service';
+import { NotFoundComponent } from './shared-components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BusinessCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NavbarComponent,
     FooterComponent,
-    CommonModule
+    CommonModule,
+    NotFoundComponent
   ],
   providers: [
     provideClientHydration(),
     provideHttpClient(),
     BusinessCardService,
+    provideAnimationsAsync(),
+    provideNativeDateAdapter(),
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    { provide: MAT_DATE_FORMATS, useValue: Environment.DATE_FORMATS },
+    ImageService
   ],
   bootstrap: [AppComponent]
 })
